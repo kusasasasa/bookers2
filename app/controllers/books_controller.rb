1bd10d53998b03255e27f4eeeb69dfeeea766ed7
kusasasasa <ec2-user@ip-172-31-21-253.ap-northeast-1.compute.internal>
2022-10-10 12:37:06 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id=current_user.id
 
-    if @book.save!
+    if @book.save
       redirect_to book_path(@book.id)
     else
       @books =Book.all
@@ -16,8 +16,13 @@ class BooksController < ApplicationController
   end
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+   
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
+   
   end
 
   def index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,12 @@ class UsersController < ApplicationController
   end
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(current_user.id)
+    if @user.update(user_params)
+      redirect_to user_path(current_user.id)
+    else
+       render :new
+    end
+      
   end
   
   def edit
